51f41763cc2a0b084e67825902b880e29ba9609c
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-07-08 19:06:29 +0000

    Automatically backed up by Learn

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   it 'has many post_category' do
-    expect(@category.post_categories).to eq([@category1, @category2])
+    expect(@category.post_categories).to eq([@post_category1, @post_category2])
   end
 end
