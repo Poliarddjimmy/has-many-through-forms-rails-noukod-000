10f95b4870256a7ca50bdf417719e985ff4a88dc
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-07-08 19:07:28 +0000

    Done.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,7 @@
-require 'rails_helper'
-
-RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# require 'rails_helper'
+#
+# RSpec.describe Category, type: :model do
+#   it 'has many post_category' do
+#     expect(@category.post_categories).to eq([@post_category1, @post_category2])
+#   end
+# end
