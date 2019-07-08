db909c143e4f5b4d17c6428a24a3e9e3bca45412
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-07-08 19:07:06 +0000

    Automatically backed up by Learn

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,5 @@
 # require 'rails_helper'
-# 
+#
 # RSpec.describe Category, type: :model do
 #   it 'has many post_category' do
 #     expect(@category.post_categories).to eq([@post_category1, @post_category2])
